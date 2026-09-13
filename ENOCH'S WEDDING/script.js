755f667f44ef5c0de977c549a02ddadc55b85e1a
e1523574-prog <e1523574@gmail.com>
2026-09-13 09:59:04 +0000

    Update script.js

--- ENOCH'S WEDDING/script.js
+++ ENOCH'S WEDDING/script.js
@@ -12,7 +12,7 @@ window.addEventListener("scroll", () => {
     if (window.scrollY > 40) {
 
         header.classList.add("scrolled");
-
+ 
     } else {
 
         header.classList.remove("scrolled");
